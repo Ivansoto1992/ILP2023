83722169ef403a0b3aec5441f22ba9c3b038de95
Ivansoto1992 <99559904+Ivansoto1992@users.noreply.github.com>
2023-01-21 12:50:54 -0600

    Add files via upload

---
+++ Operaciones Matematicas.py
@@ -0,0 +1,49 @@
+#Operaciones Matematicas
+
+#entrada de datos 
+#Declarar o crear dos variables 
+numero_1 = 10
+numero_2 = 2
+
+#Constante
+Gravedad_Tierra = 9.8
+PI = 3.1416
+
+
+#Procesos (Operaciones o calculos Matematicos y/o Logicos)
+suma = numero_1 + numero_2
+resta = numero_1 - numero_2
+suma = 0 
+multiplicar = numero_1 * numero_2
+division = numero_2 / numero_2
+
+
+potencia_1 = numero_1 ** numero_2
+potencia_2 = pow(numero_1, numero_2)
+cuadrado = numero_1 ** 2
+cubo = pow(numero_2, 3)
+
+
+#Salida de Datos
+print("La suma es igual a", suma)
+print("La resta es igual a", resta)
+print('La suma es igual a' + str (suma))
+#concatenar (union de 2 o mas textos)
+#Casteo: Conversion de un tipo de dato en otro tipo de dato
+
+print(f"La suma es igual a {suma}") #d: formateo de interpolacion de texto 
+
+#constante=valor que se mantiene fijo o no cambia su valor
+
+#Syntax= elemento que puede cambiar su valor 
+#nombre_variable= valor 
+
+print("La potencia 1 =", potencia_1)
+print("La potencia 2 =", potencia_2)
+
+# raiz_cuadrada_1 = 
+# raiz_cuadrada_2 = 
+
+# modulo_residuo = 
+
+print("redondear la suma de constantes = ", round(Gravedad_Tierra + PI, 2))
