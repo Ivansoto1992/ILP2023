15ac9597e27d53993753a237c7fd6980a73e1067
Ivansoto1992 <99559904+Ivansoto1992@users.noreply.github.com>
2023-02-11 10:43:48 -0600

    Add files via upload

---
+++ Ejercicio6.py
@@ -0,0 +1,12 @@
+#EJ 6. Pedir un número y decir si es par o impar.
+
+#entrada
+
+numero_a= int(input("pedir un numero: "))
+
+#procesos
+
+if numero_a % 2==0:
+    print("es un numero par: ")
+else:
+    print("es un numero impar: ")
